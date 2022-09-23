edccadc4f0253cea45e361a53bb4ac5d24511af1
Samuel Kyletoft <samuel@kyletoft.se>
2022-09-24 00:23:16 +0200

    Clean rule, link rule, full file paths

--- Makegen.hs
+++ Makegen.hs
@@ -1,8 +1,14 @@
+import           Control.Monad       (mapM)
 import           Control.Monad.Extra (concatMapM)
 import           Data.List           (elemIndex)
+import           Data.Maybe          (catMaybes, mapMaybe)
 import           System.Directory    (doesDirectoryExist, listDirectory)
 
 todo = error "Todo"
+
+data RuleInfo =
+  RuleInfo String String String String
+  deriving (Show, Eq)
 
 orElse :: a -> Maybe a -> a
 orElse _ (Just x) = x
@@ -10,23 +16,24 @@ orElse x _        = x
 
 outputType ext =
   case ext of
-    "erl"  -> "beam"
-    "ts"   -> "js"
-    "java" -> "class"
-    "cs"   -> "dll"
-    "fs"   -> "dll"
-    _      -> "o"
+    "erl"   -> "beam"
+    "ts"    -> "js"
+    "java"  -> "class"
+    "scala" -> "class"
+    "cs"    -> "dll"
+    "fs"    -> "dll"
+    _       -> "o"
 
 compileCommand ext =
   case ext of
-    "hs"    -> Just "ghc $(HASKELL_FLAGS) "
-    "c"     -> Just "cc $(CFLAGS) "
-    "cpp"   -> Just "c++ $(CXXFLAGS) "
-    "rs"    -> Just "rustc $(RUST_FLAGS) "
-    "erl"   -> Just "erl -compile $(ERLANG_FLAGS) "
-    "java"  -> Just "javac $(JAVA_FLAGS) "
-    "scala" -> Just "scalac $(SCALA_FLAGS) "
-    "cs"    -> Just "rosyln $(CS_FLAGS) "
+    "hs"    -> Just "ghc $(HASKELL_FLAGS) -c -o "
+    "c"     -> Just "cc $(CFLAGS) -c -o "
+    "cpp"   -> Just "c++ $(CXXFLAGS) -c -o "
+    "rs"    -> Just "rustc $(RUST_FLAGS) -c -o "
+    "erl"   -> Just "erl -compile $(ERLANG_FLAGS) -o "
+    "java"  -> Just "javac $(JAVA_FLAGS) -o "
+    "scala" -> Just "scalac $(SCALA_FLAGS) -o "
+    "cs"    -> Just "rosyln $(CS_FLAGS) -o "
     _       -> Nothing
 
 splitName :: String -> (String, String)
@@ -35,24 +42,59 @@ splitName name = splitAt finalDot name
     reverseLength x = length name - x
     finalDot = orElse 0 . fmap reverseLength . elemIndex '.' . reverse $ name
 
-generateMakeRule :: String -> String
-generateMakeRule filename =
-  case compiler of
-    Just comp -> concat [output, ": ", filename, "\n\t", comp, filename]
-    Nothing   -> ""
+printGenerateMakeRule :: RuleInfo -> String
+printGenerateMakeRule (RuleInfo input compiler output _) =
+  concat [output, ": ", input, "\n\t", compiler, output, " ", input, "\n"]
+
+makeRule :: String -> Maybe RuleInfo
+makeRule filename =
+  fmap (\comp -> RuleInfo filename comp output outExt) compiler
   where
     (name, ext) = splitName filename
-    output = name ++ outputType ext
+    outExt = outputType ext
+    output = name ++ outExt
     compiler = compileCommand ext
 
 getRecursiveFiles :: FilePath -> IO [FilePath]
 getRecursiveFiles path = do
   isFolder <- doesDirectoryExist path
+  let addPathTo file = path ++ '/' : file
   if isFolder
-    then concatMapM getRecursiveFiles =<< listDirectory path
+    then fmap (map addPathTo) . concatMapM getRecursiveFiles =<<
+         listDirectory path
     else return [path]
 
+stripThisFolder :: FilePath -> FilePath
+stripThisFolder ('.':'/':xs) = xs
+stripThisFolder xs           = xs
+
+join :: String -> [String] -> String
+join mid (a:b:cs) = a ++ mid ++ join mid (b : cs)
+join _ [a]        = a
+join _ []         = []
+
+cleanRule :: [RuleInfo] -> String
+cleanRule =
+  ("clean:\n\t-rm link\n" ++) .
+  unlines . map (("\t-rm " ++) . (\(RuleInfo _ _ a _) -> a))
+
+linkRule :: [RuleInfo] -> String
+linkRule rules = concat ["link: ", inputs, "\n\tcc ", inputs, " -o link"]
+  where
+    inputs =
+      unwords .
+      map (\(RuleInfo _ _ output _) -> output) .
+      filter (\(RuleInfo _ _ _ ext) -> ext == "o") $
+      rules
+
+phony :: String
+phony = ".PHONY:\n\tclean"
+
 main :: IO ()
-main =
-  putStrLn . unlines  =<<
-  getRecursiveFiles "."
+main = do
+  files <- fmap (map stripThisFolder) . getRecursiveFiles $ "."
+  let rules = mapMaybe makeRule files
+  let clean = cleanRule rules
+  let link = linkRule rules
+  putStrLn . unlines . (++ [link, clean, phony]) . map printGenerateMakeRule $
+    rules
